ad8001afb0c09a6fb1b4a398cb2691268e0cdc0b
changwenwu <wucw5513@mails.jlu.edu.cn>
2021-06-02 17:07:42 +0800

    feat: Init Commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { hot } from 'react-hot-loader/root';
+import Home from './pages/home';
+
+function App() {
+    return (
+        <div>
+            <Home />
+        </div>
+    );
+}
+
+export default hot(App);
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+    return <div>React Recoil Starter</div>;
+};
+
+export default Home;
